1f16dc2cccd82230c08c7ee7dd8e51a82841fbb4
Laurie <>
2024-05-13 06:08:01 +0200

    Instrumentation: add support for software breakpoints

--- win_agent/instrumentation/fault_inducer.cpp
+++ win_agent/instrumentation/fault_inducer.cpp
@@ -1,49 +1,94 @@
+#include "fault_inducer.hpp"
 #include "meta.hpp"
 #include "veh.hpp"
 #include <function_finder/functions.hpp>
-#include <functional>
 #include <optional>
 #include <windows.h>
 
 namespace ada {
 
-struct SoftwareInstrumentation {
-  void *begin, *end;
-  u64 original_protection;
+std::optional<MEMORY_BASIC_INFORMATION>
+set_protect(void *begin, void *end, u64 (*change_prot)(u64 prot)) {
+  // set page_guard flag
+  MEMORY_BASIC_INFORMATION prot;
+  if (VirtualQuery(begin, &prot, sizeof(prot)) == 0)
+    return std::nullopt;
 
-  SoftwareInstrumentation(void *begin, void *end, u64 original_protection)
-      : begin(begin), end(end), original_protection(original_protection) {}
+  DWORD oldprot = 0;
+  ASSERT(end > begin);
+  u64 size = (u64)end - (u64)begin;
 
-  bool triggered_violation(Violation const &violation) const {
-    return violation.fault_address >= begin && violation.fault_address < end;
+  if (VirtualProtect(begin, size, change_prot(prot.Protect), &oldprot) == 0)
+    return std::nullopt;
+
+  return prot;
+}
+
+bool InstrumentationHandler::instrument(ada::Function const &fn) {
+  auto instr = CodeInstrumentation::install(fn.start);
+  if (!instr.has_value())
+    return false;
+
+  instrumented_instructions.emplace_back(*instr);
+  instr->enable();
+  return true;
+}
+
+CodeInstrumentation *
+InstrumentationHandler::find_code_instrumentation(Violation const &v,
+                                                  _EXCEPTION_POINTERS *eh) {
+  auto instrumentation_hit = std::find_if(
+      instrumented_instructions.begin(), instrumented_instructions.end(),
+      [&v](CodeInstrumentation const &instr) {
+        return instr.instruction == v.ip;
+      });
+
+  if (instrumentation_hit == instrumented_instructions.end())
+    return nullptr;
+
+  return &(*instrumentation_hit);
+}
+
+void CodeInstrumentation::enable() {
+  // Fix the permission.
+  VirtualProtect(instruction, 1, PAGE_EXECUTE_READWRITE,
+                 (DWORD *)&this->original_protection);
+
+  // write INT3
+  *(u8 *)instruction = 0xCC;
+
+  DWORD prot;
+  VirtualProtect(instruction, 1, this->original_protection, &prot);
+}
+
+void CodeInstrumentation::disable() {
+  // Fix the permission.
+  VirtualProtect(instruction, 1, PAGE_EXECUTE_READWRITE,
+                 (DWORD *)&this->original_protection);
+
+  // Restore original byte
+  *(u8 *)instruction = original_byte;
+
+  DWORD prot;
+  VirtualProtect(instruction, 1, this->original_protection, &prot);
+}
+
+std::optional<CodeInstrumentation> CodeInstrumentation::install(void *instr) {
+  CodeInstrumentation result;
+  result.instruction = instr;
+
+  MEMORY_BASIC_INFORMATION prot;
+  if (VirtualQuery(instr, &prot, sizeof(prot)) == 0)
+    return std::nullopt;
+
+  // can't execute anyway, this surely is a bad call to `install()`
+  if (prot.Protect != PAGE_EXECUTE && prot.Protect != PAGE_EXECUTE_READ &&
+      prot.AllocationProtect != PAGE_EXECUTE_READWRITE) {
+    return std::nullopt;
   }
 
-  virtual void on_execute() {}
-
-  static std::optional<SoftwareInstrumentation> install_singlehit(void *begin,
-                                                                  void *end) {
-    ASSERT(end > begin);
-
-    // set page_guard flag
-    MEMORY_BASIC_INFORMATION prot;
-    if (VirtualQuery(begin, &prot, sizeof(prot)) == 0)
-      return std::nullopt;
-
-    auto newprot = prot.Protect | PAGE_GUARD;
-    DWORD oldprot = 0;
-
-    u64 size = (((u64)end - (u64)begin) & ~(0x1000 - 1)) + 0x1000;
-
-    VirtualProtect(begin, size, newprot, &oldprot);
-    return SoftwareInstrumentation{begin, end, oldprot};
-  }
-};
-
-struct InstrumentationHandler {
-  std::vector<SoftwareInstrumentation> instrumented_functions;
-
-  void handle_fault();
-
-  
-};
+  result.original_byte = *(u8 *)instr;
+  result.original_protection = prot.AllocationProtect;
+  return result;
+}
 } // namespace ada
---
+++ win_agent/instrumentation/fault_inducer.hpp
@@ -0,0 +1,39 @@
+#include "meta.hpp"
+#include <optional>
+#include <vector>
+
+// windows.h
+struct _EXCEPTION_POINTERS;
+
+namespace ada {
+struct Violation;
+struct Function;
+
+// Represents a software breakpoint using int3
+struct CodeInstrumentation {
+  // instruction to be breakpointed
+  void *instruction;
+  // original byte that's been replaced with 0xCC
+  u8 original_byte;
+  // original protection flags returned by `VirtualQuery()`
+  u32 original_protection;
+
+  // attempts to set up a new CodeInstruction at |instr|
+  static std::optional<CodeInstrumentation> install(void *instr);
+
+  // writes INT3 to |instruction|
+  void enable();
+  // restores |instruction| to |original_byte|
+  void disable();
+};
+
+struct InstrumentationHandler {
+  std::vector<CodeInstrumentation> instrumented_instructions;
+
+  bool instrument(ada::Function const &fn);
+  CodeInstrumentation *find_code_instrumentation(Violation const &v,
+                                                 _EXCEPTION_POINTERS *eh);
+};
+
+inline InstrumentationHandler global_instrumentations;
+} // namespace ada
